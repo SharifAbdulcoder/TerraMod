17c4d24eef48fcefb16ef11bc7204c97140b5faf
SharifAbdulcoder <43179042+SharifAbdulcoder@users.noreply.github.com>
2018-11-05 18:03:06 -0600

    Add files via upload

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ module "ec2_cluster" {
 
   ami                    = "ami-0b59bfac6be064b78"
   instance_type          = "t2.micro"
-  key_name               = "user1"
+  key_name               = "SSHKEY"
   monitoring             = true
   vpc_security_group_ids = ["sg-0bc39239ec5b00e85"]
   subnet_id              = "subnet-32d8207e"
@@ -16,4 +16,26 @@ module "ec2_cluster" {
     Terraform   = "true"
     Environment = "dev"
   }
+}
+
+module "web_server_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "Terraform"
+  description = "Terraform"
+  vpc_id      = "vpc-5d0e3d35"
+
+  ingress_cidr_blocks = ["10.10.0.0/16"]
+}
+
+module "security-group" {
+  source      = "terraform-aws-modules/security-group/aws"
+  version     = "2.9.0"
+  name        = "Terraform"
+  description = "Security group for Terraform"
+  vpc_id      = ""
+
+  ingress_cidr_blocks = ["10.10.0.0/16"]
+
+  # insert the 2 required variables here
 }
